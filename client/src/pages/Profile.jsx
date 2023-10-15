cb3defebd700f5c466503202a21419fb879d4b85
esfredson <federicofrasquet@gmail.com>
2023-10-15 11:21:48 +0200

    complete delete user listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -146,11 +146,29 @@ export default function Profile() {
           setShowListingsError(true);
           return;
         }
-
         setUserListings(data);
 
     } catch (error) {
       setShowListingsError(true);
+    }
+  };
+
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: "DELETE",
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        console.log(data.message);
+        return;
+      }
+
+      setUserListings((prev) =>
+        prev.filter((listing) => listing._id !== listingId)
+      );
+    } catch (error) {
+      console.log(error.message);
     }
   };
 
@@ -272,13 +290,12 @@ export default function Profile() {
               <div className='flex flex-col item-center'>
                 <button
                   onClick={() => handleListingDelete(listing._id)}
-                  className='text-red-700 uppercase'
-                >
-                  Delete
+                  className='text-red-700 uppercase'>Delete
+                </button>             
+                <button
+                  className='text-green-700 uppercase'>Edit
                 </button>
-                <Link to={`/update-listing/${listing._id}`}>
-                  <button className='text-green-700 uppercase'>Edit</button>
-                </Link>
+                
               </div>
             </div>
           ))}
